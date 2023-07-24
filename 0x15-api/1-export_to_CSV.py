db621ac9536e3c286ec4069ea2c1af37091fb29b
Messai <messai@github.com>
2023-07-24 19:17:12 +0300

    Added 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,6 +4,7 @@ about his/her TODO list progress
 """
 import requests
 import sys
+import csv
 
 
 if __name__ == '__main__':
@@ -11,7 +12,6 @@ if __name__ == '__main__':
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    with open("{}.csv".format(sys.argv[1]), 'w') as f:
+        obj = csv.writer(f)
+        [obj.writerow([user['id'], user['name'], t['completed'], t['title']])for t in todos]
