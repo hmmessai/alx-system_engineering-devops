3f815978f5f1ef274584b2bbc65d55cdde8e3f11
Messai <messai@github.com>
2023-08-08 15:59:28 +0300

    requests useragent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,6 +9,9 @@ def number_of_subscribers(subreddit):
     """Return the number of subscribers of the given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
+    headers = {
+            "User-Agent": "Python-requests/2.25.1"     
+    }
     response = requests.get(url, allow_redirects=False)
     if response.status_code == 404:
         return 0
